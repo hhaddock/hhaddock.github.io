10c8839c1ec8801f26d7985e1c035c9e9de09cab
hhaddock <haydenhaddock1@gmail.com>
2017-02-06 14:14:48 -0600

    test

--- js/function.js
+++ js/function.js
@@ -10,21 +10,23 @@ function startTime() {
 var today = new Date();
 var h = today.getHours();
 var m = today.getMinutes();
-var s = today.getSeconds();
 var t = amPm(h);
-m = checkTime(m);
-h = checkTime(h);
-s = checkTime(s);
+m = checkMin(m);
+h = checkHr(h);
 
 document.getElementById('time').innerHTML =
   h + ":" + m + " " + t;
   var t = setTimeout(startTime, 500);
 }
 
-function checkTime(i, s) {
+function checkMin(i) {
   if (i < 10) {
     i = "0" + i
   }  // add zero in front of numbers < 10
+  return i;
+}
+
+function checkHr(i){
   if(i > 12 && i <= 24){
     i = i-12;
   }
